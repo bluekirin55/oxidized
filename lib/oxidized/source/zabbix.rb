5ce0c7826460cb5b8eeee1654a3ddff817095bf5
bluekirin55 <106004377+bluekirin55@users.noreply.github.com>
2022-10-15 19:46:08 +0900

    add zabbix support
    1. zbx_template_oxidized.yamlをZabbixにインポート
    2. "Template Oxidized"を対象のホストに適用
    3. ホストのマクロをOxidized用に設定
     {$OXIDIZED_MODEL}
     {$OXIDIZED_USERNAME}
     {$OXIDIZED_PASSWORD}
    4. Oxidizedのconfigで以下を設定
      source:
        default: zabbix
        debug: true
        zabbix:
          url: "http://your_zabbix/api_jsonrpc.php"
          user: "Admin"
          password: "zabbix"
          template: "Template Oxidized"
          map:
            username: "{$OXIDIZED_USERNAME}"
            password: "{$OXIDIZED_PASSWORD}"
            model: "{$OXIDIZED_MODEL}"
          vars_map:
            enable: "{$OXIDIZED_ENABLE}"

---
+++ lib/oxidized/source/zabbix.rb
@@ -0,0 +1,192 @@
+module Oxidized 
+
+  class Zabbix < Source
+
+    def initialize
+      @cfg = Oxidized.config.source.zabbix
+      super
+    end
+
+    def setup
+      return unless @cfg.empty?
+
+      raise NoConfig, 'no source l config, edit ~/.config/oxidized/config'
+    end
+
+    require "net/http"
+    require "json"
+
+    def load(node_want = nil)
+
+      token = zabbix_user_login(@cfg.user.to_s, @cfg.password.to_s)
+      result = zabbix_template_get(@cfg.template, token)
+
+      if result.length == 0
+        exit
+      end
+
+
+      hostids = []
+      result[0]["hosts"].each do | host |
+        hostids.push(host["hostid"])
+      end
+
+      hosts = zabbix_host_get(hostids, token)
+      ifaces = zabbix_hostinterface_get(hosts.keys, token)
+      macros = zabbix_usermacro_get(hosts.keys, token)
+
+      nodes = []
+      hosts.keys.each do | hostid |
+        node = {}
+        node.update(hosts[hostid])  if hosts.has_key?(hostid)
+        node.update(ifaces[hostid]) if ifaces.has_key?(hostid)
+        node.update(macros[hostid]) if macros.has_key?(hostid)
+
+        node[:model] = map_model node[:model] if node.has_key? :model
+        node[:group] = map_group node[:group] if node.has_key? :group
+
+        nodes.push(node)
+      end
+      nodes
+
+    end
+
+    private
+
+    def zabbix_api(method, params, token = nil)
+      uri = URI.parse(@cfg.url.to_s)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      if uri.scheme == 'https'
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+
+      response = http.post(
+        uri.request_uri,
+        {
+          jsonrpc: "2.0",
+          id: "0",
+          method: method,
+          params: params,
+          auth: token
+        }.to_json,
+        { "Content-Type" => "application/json-rpc" }
+      )
+
+      JSON.parse(response.body)["result"]
+    end
+
+    def zabbix_user_login(user, password)
+      zabbix_api(
+        "user.login",
+        {
+          user: user,
+          password: password
+        }
+      )
+    end
+
+    def zabbix_template_get(name, token)
+      zabbix_api(
+        "template.get",
+        {
+          output: "hosts",
+          selectHosts: [],
+          filter: {
+            host: name
+          }
+        },
+        token
+      )
+    end
+
+    def zabbix_host_get(hostids, token)
+      result = zabbix_api(
+        "host.get",
+        {
+          output: ["hostid", "host", "name"],
+          hostids: hostids,
+          filter: {
+            status: "0"
+          }
+        },
+        token
+      )
+
+      items = {}
+      result.each do | item |
+        hostid = item["hostid"]
+        items[hostid] = {} if not items.has_key?(hostid)
+
+        items[hostid][:name] = node_var_interpolate(item["host"])
+      end
+      items
+    end
+
+    def zabbix_hostinterface_get(hostids, token)
+      result = zabbix_api(
+        "hostinterface.get",
+        {
+          output: ["hostid", "useip", "ip", "dns"],
+          hostids: hostids,
+          filter: {
+            available: "1"
+          }
+        },
+        token
+      )
+
+      items = {}
+      result.each do | item |
+        hostid = item["hostid"]
+        items[hostid] = {} if not items.has_key?(hostid)
+
+        items[hostid][:ip] = item["useip"] == "1" ? node_var_interpolate(item["ip"]) : node_var_interpolate(item["dns"])
+      end
+      items
+    end
+
+    def zabbix_usermacro_get(hostids, token)
+
+      macronames = []
+      @cfg.map.each do | k, v |
+        macronames.push(v.to_s)
+      end
+      @cfg.vars_map.each do | k, v |
+        macronames.push(v.to_s)
+      end
+
+      result = zabbix_api(
+        "usermacro.get",
+        {
+          output: ["hostid", "macro", "value"],
+          hostids: hostids,
+          filter: {
+            macro: macronames
+          }
+        },
+        token
+      )
+
+      items = {}
+      result.each do | item |
+        hostid = item["hostid"]
+        items[hostid] = {} unless items.has_key?(hostid)
+
+        @cfg.map.each do | k, v |
+          items[hostid][k.to_sym] = node_var_interpolate(item["value"]) if item["macro"] == v.to_s
+        end
+
+        items[hostid][:vars] = {} unless items[hostid].has_key?(:vars)
+        @cfg.vars_map.each do | k, v |
+          items[hostid][:vars][k.to_sym] = node_var_interpolate(item["value"]) if item["macro"] == v.to_s
+        end
+
+      end
+      items
+    end
+
+  end
+
+end
